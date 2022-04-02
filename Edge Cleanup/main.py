ac2620a92b13afe0e4ee82de860a77a327b9c5f6
AndrewJAHogue <andrewjahogue@gmail.com>
2022-04-01 23:59:27 -0500

    Achieved a working method to target the edges of the data using exposure times

--- Edge Cleanup/main.py
+++ Edge Cleanup/main.py
@@ -8,6 +8,10 @@ from astropy.io import fits
 import lineplots as plt
 import filetree as ft
 import stars as st
+
+import time
+start = time.perf_counter_ns()
+
 parent = ft.FitsFolder()
 sofia = parent + 'Full Maps/F0217_FO_IMA_70030015_FORF253_MOS_0001-0348_final_MATT_Corrected.fits'
 spit = parent + 'Full Maps/Spitzer_GCmosaic_24um_onFORCASTheader_JyPix.fits'
@@ -22,15 +26,27 @@ f_exposure = fits_hdu[2].data
 # t_max = 793
 copy = fits_hdu[0].data.copy()
 for why, y in enumerate(copy):
-    for ex,x in enumerate(y):
+    t_max = np.nanmax(f_exposure[why])
+    for ex,x in enumerate(copy[why]):
         t_ex = f_exposure[why][ex]
-        t_max = np.max(y)
-        if t_ex < 0.3*t_max:
-            x = np.nan
-
+        # print(f'The exposure for x:{ex} and y:{why} is {t_ex} and the max for this row is {t_max}')
+        if t_ex < 0.3*t_max and np.isnan(t_max) == False or t_ex == 0.0:
+            copy[why][ex] = np.nan
+# t_max = np.nanmax(f_exposure[180])
+# for i, x in enumerate(copy[180]):
+#     t = f_exposure[180][i]
+#     if t < 0.3*t_max:
+#         copy[180][i] = np.nan
 
 filename = new_path + 'tmp_sofia.fits'
 fits.writeto(filename, copy, fits_hdu[0].header, overwrite=True)
-print(f'File written at {new_path}')
+print(f'File written at {filename}')
+end = time.perf_counter_ns()
 
-# print(np.max(f_exposure[4716]))
+# print((f_exposure[180][f_exposure[180] < 160]))
+
+
+
+
+
+print(f'Function processed in {((end - start)*1e-6)} ms')
