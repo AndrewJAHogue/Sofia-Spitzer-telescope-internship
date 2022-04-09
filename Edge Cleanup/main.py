9df02c1b3f57c474f85ab7423bd1f773eeeef1f5
AndrewJAHogue <andrewjahogue@gmail.com>
2022-04-09 18:34:11 -0500

    Moved to working on full files, only limited how much data gets processed at. Looking at small sections of the map first, to test edge masking, so as to save time.

--- Edge Cleanup/main.py
+++ Edge Cleanup/main.py
@@ -25,49 +25,35 @@ f_var = fits_hdu[1].data
 f_exposure = fits_hdu[2].data
 # t_max = 793
 threshold = 85
-copy = fits_hdu[0].data.copy()
-odd_stamps = [
-    copy[2798:3070][0:271].copy(),
-    copy[3656:3719][0:63].copy(),
-    copy[3510:3570][:60].copy()
-]
+# this [:605] limits it to essentially isofield1, for efficiency
+copy = fits_hdu[0].data[:605].copy()
+
+fraction = .5
+
 for why, y in enumerate(copy):
-    no_zeros = f_exposure[f_exposure != 0].copy()
-    t_max = np.nanmedian(no_zeros[why])
+    t_max = np.nanmedian(f_exposure[f_exposure != 0][why])
     for ex,x in enumerate(copy[why]):
         t_ex = f_exposure[why][ex]
-        # print(f'The exposure for x:{ex} and y:{why} is {t_ex} and the max for this row is {t_max}')
-        # if why < 3634 and x < 1841 or why > 3814 and x > 1807:
-        if t_ex < 0.55*t_max and np.isnan(t_max) == False or t_ex == 0.0:
+        if t_ex < fraction*t_max and np.isnan(t_max) == False or t_ex == 0.0:
             copy[why][ex] = np.nan
-#             if why < 2803:
-#                 if ex < 2053:
-#                     copy[why][ex] = np.nan
-#             if why > 3060 and why < 3200:
-#                 if ex < 2053:
-#                     copy[why][ex] = np.nan
-#             if why > 3200 and why < 3725:
-#                 if ex > 2260:
-#                     copy[why][ex] = np.nan
-copy[2798:3070][0:271] = odd_stamps[0]
-copy[3656:3719][0:63] = odd_stamps[1]
-copy[3510:3570][:60] = odd_stamps[2]
 
-filename = new_path + 'tmp_sofia.fits'
+filename = new_path + f'{fraction}_isoOne.fits'
 
 fits.writeto(filename, copy, fits_hdu[0].header, overwrite=True)
 
 print(f'File written at {filename}')
+
+
+
+
+
+
 end = time.perf_counter_ns()
-
-
-
-
 time = end - start
 if time / 1e6 < 1000:
     time /= 1e6
     time = str(time) + ' ms'
-elif time / 1e9 < 360:
+elif time / 1e9 < 120:
     time /= 1e9
     time = str(time) + ' s'
 print(f'Function processed in {time}')
